9d1c846d1173ab9af61dee46220e88e58448d36a
coding.moh@gmail.com <coding.moh@gmail.com>
2021-12-07 15:25:52 +0100

    init

--- hello-world/background.sh
+++ hello-world/background.sh
@@ -1,3 +1,5 @@
 #/bin/bash
 
 git clone https://github.com/codingmoh/fhtw-hello-world.git
+git clone https://github.com/codingmoh/dsc_streamlit.git
+git clone https://github.com/codingmoh/dsc-sdc.git
